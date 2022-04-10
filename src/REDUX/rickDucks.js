3532d7b44966eab68a693ef96a5f9b62a5ed6614
Daniela Castro Salazar <88462620+DaniCastro-developer@users.noreply.github.com>
2022-04-09 22:09:02 -0400

    cambios al local storage prev y next

--- src/REDUX/rickDucks.js
+++ src/REDUX/rickDucks.js
@@ -3,59 +3,100 @@ import axios from "axios"
 // constants
 
 const dataInitial = {
-    array: [],
-    offset: 0
+    info: [],
+    results: []
 }
 
 // types
 const GET_DATA_SUCESS = 'GET_DATA_SUCESS'
 const NEXT_DATA_SUCESS = 'NEXT_DATA_SUCESS'
-
+const PREVIOUS_DATA_SUCESS = 'PREVIOUS_DATA_SUCESS'
 
 //reducer
 export default function RickReducer (state = dataInitial, action) {
     switch(action.type){
        case GET_DATA_SUCESS:
-           return {...state, array: action.payload}
+           return {...state, ...action.payload}
         case NEXT_DATA_SUCESS:
-           return {...state, array: action.payload.array, offset: action.payload.offset}
-           default:
+           return {...state, ...action.payload}
+        case PREVIOUS_DATA_SUCESS:
+            return {...state, ...action.payload}
+        default:
             return state
     }
 }
 
 //accions
 export const getDataAccion = () => async (dispatch, getState) => {
-
    // console.log('getState', getState().rickAndMorty.offset)
-    const {offset} = getState().rickAndMorty;
-
+   
+   if(localStorage.getItem('arrayData')){
+    console.log('exist')
+    dispatch({
+        type: GET_DATA_SUCESS,
+        payload: JSON.parse(localStorage.getItem('arrayData'))
+    })
+} else {
+    console.log('do not exist')
     try {
-        const res = await axios.get(`https://rickandmortyapi.com/api/character?page=${offset}`)
+        const res = await axios.get(`https://rickandmortyapi.com/api/character?page=0`)
+        //console.log(res.data)
         dispatch({
             type: GET_DATA_SUCESS,
-            payload: res.data.results
+            payload: res.data
         })
+        localStorage.setItem('arrayData', JSON.stringify(res.data))
     } catch (error) {
         console.log(error)
     }
 
 }
+}
 
 export const nextPageDataAccion = () => async (dispatch, getState) => {
 
-    const {offset} = getState().rickAndMorty;
-    const next = offset + 1
-    try {
-        const res = await axios.get(`https://rickandmortyapi.com/api/character?page=${offset}`)
+    const {next} = getState().rickAndMorty.info;
+    //console.log(next)
+
+    if(localStorage.getItem(next)){
+        console.log('exist')
         dispatch({
             type: NEXT_DATA_SUCESS,
-            payload: {
-                array: res.data.results, 
-                offset: next
-            }
+            payload: JSON.parse(localStorage.getItem(next))
+        })
+    } else {
+    try {
+        const res = await axios.get(next)
+        dispatch({
+            type: NEXT_DATA_SUCESS,
+            payload: res.data
+        })
+        localStorage.setItem(next, JSON.stringify(res.data))
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+}
+
+export const previousPageDataAccion = () => async (dispatch, getState) => {
+
+    const {prev} = getState().rickAndMorty.info;
+    if(localStorage.getItem(prev)){
+        console.log('previo')
+        dispatch({
+            type: PREVIOUS_DATA_SUCESS,
+            payload: JSON.parse(localStorage.getItem(prev))
+        })
+    } else {
+    try {
+        const res = await axios.get(prev)
+        dispatch({
+            type: PREVIOUS_DATA_SUCESS,
+            payload: res.data
         })
     } catch (error) {
         console.log(error)
     }
+}
 }
--- src/components/RickAndMorty.js
+++ src/components/RickAndMorty.js
@@ -1,26 +1,27 @@
-// prueba de commit 
+// prueba de commit
 import React from "react";
 
 import { useDispatch, useSelector } from "react-redux";
-import { getDataAccion, nextPageDataAccion } from "../REDUX/rickDucks";
+import {
+  getDataAccion,
+  nextPageDataAccion,
+  previousPageDataAccion,
+} from "../REDUX/rickDucks";
 
 const RickAndMorty = () => {
   const dispatch = useDispatch();
 
-  const readData = useSelector((store) => store.rickAndMorty.array);
+  const readData = useSelector((store) => store.rickAndMorty.results);
+  const { next } = useSelector((store) => store.rickAndMorty.info);
+  const { prev } = useSelector((store) => store.rickAndMorty.info);
+
   //console.log(readData)
 
   return (
     <div>
       <h1 className="text-center">Rick and Morty list</h1>
 
-      {readData.map((item, id) => (
-        <div key={id} className="card">
-          <div className="card-body">{item.name}</div>
-        </div>
-      ))}
-
-      <div className="d-grid gap-2 d-md-block">
+      {readData.length === 0 && (
         <button
           className="btn btn-primary"
           type="button"
@@ -29,14 +30,35 @@ const RickAndMorty = () => {
           {" "}
           Obtener Data{" "}
         </button>
-        <button
-          className="btn btn-primary"
-          type="button"
-          onClick={() => dispatch(nextPageDataAccion())}
-        >
-          {" "}
-          Siguiente{" "}
-        </button>
+      )}
+
+      {readData.map((item, id) => (
+        <div key={id} className="card">
+          <div className="card-body">{item.name}</div>
+        </div>
+      ))}
+
+      <div className="d-grid gap-2 d-md-block mt-3">
+        {prev && (
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => dispatch(previousPageDataAccion())}
+          >
+            {" "}
+            Anterior{" "}
+          </button>
+        )}
+        {next && (
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => dispatch(nextPageDataAccion())}
+          >
+            {" "}
+            Siguiente{" "}
+          </button>
+        )}
       </div>
     </div>
   );
